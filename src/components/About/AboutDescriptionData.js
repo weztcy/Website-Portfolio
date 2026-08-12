fc84fb9a463bea51c4e2eac86e6bfd34469e7f7e
Dimas Pratama <yohanesdimas888@gmail.com>
2026-08-12 16:40:48 +0700

    Update

--- src/components/About/AboutDescriptionData.js
+++ src/components/About/AboutDescriptionData.js
@@ -7,12 +7,12 @@ const AboutDescriptionData = [
   {
     id: 2,
     description:
-      "Throughout my academic and professional journey, I have developed skills in Data Analytics, Web Development, Machine Learning, and UI/UX Design. I enjoy transforming complex problems into efficient and meaningful digital solutions by combining technical knowledge with analytical thinking and creativity.",
+      "Throughout my academic and professional journey, I have developed skills in Data Analytics, Web Development, Machine Learning, and UI/UX Design.",
   },
   {
     id: 3,
     description:
-      "I have experience working on web development projects, data-driven tasks, and various digital solutions. These experiences have helped me develop not only technical abilities but also strong communication, problem-solving, teamwork, and adaptability skills.",
+      "I enjoy transforming complex problems into efficient and meaningful digital solutions by combining technical expertise, analytical thinking, and creativity. My experience in web development, data-driven projects, and digital solutions has also strengthened my communication, problem-solving, teamwork, and adaptability skills.",
   },
   {
     id: 4,
