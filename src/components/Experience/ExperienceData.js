f4d8b2b673ec1f1b8638e487ac0a8cbf528fc35f
Dimas Pratama <yohanesdimas888@gmail.com>
2026-08-26 14:08:13 +0700

    Update

--- src/components/Experience/ExperienceData.js
+++ src/components/Experience/ExperienceData.js
@@ -3,26 +3,6 @@ import freelance_copywriter from "../../assets/experience/freelance_copywriter.p
 import bank_mandiri from "../../assets/experience/bank_mandiri.png";
 
 const experienceData = [
-  // {
-  //   id: 1,
-
-  //   company: "Dinas Kependudukan dan Pencatatan Sipil Kota Semarang",
-
-  //   logo: disdukcapilLogo,
-
-  //   position: "Web Developer",
-
-  //   duration: "August 2024 - October 2024",
-
-  //   location: "Semarang, Indonesia",
-
-  //   responsibilities: [
-  //     "Designed the UI/UX of the PRAJAGAMER website to enhance user experience and improve user interaction.",
-  //     "Developed the PRAJAGAMER website as an internship registration platform, streamlining the registration process and improving registration efficiency by 60%.",
-  //     "Managed hardware inventory and handled administrative tasks to support daily operations and ensure optimal equipment availability.",
-  //   ],
-  // },
-
   {
     id: 1,
 
@@ -37,31 +17,11 @@ const experienceData = [
     location: "Semarang, Indonesia",
 
     responsibilities: [
-      "Served approximately 50–150 customers per day for cash deposits, withdrawals, transfers, payments, and other banking services, maintaining a 99% transaction accuracy rate in accordance with Bank Mandiri procedures.",
-      "Processed approximately 100–250 daily transactions while ensuring service efficiency, transaction security, and consistency between system records and supporting documents.",
-      "Responsible for transaction verification, reconciliation, and routine financial reporting, maintaining report accuracy, ensuring transaction data consistency, and supporting smooth audit processes.",
+      "Designed the UI/UX of the PRAJAGAMER website to enhance user experience and improve user interaction.",
+      "Developed the PRAJAGAMER website as an internship registration platform, streamlining the registration process and improving registration efficiency by 60%.",
+      "Managed hardware inventory and handled administrative tasks to support daily operations and ensure optimal equipment availability.",
     ],
   },
-
-  // {
-  //   id: 2,
-
-  //   company: "Freelance",
-
-  //   logo: freelance_copywriter,
-
-  //   position: "Copywriter",
-
-  //   duration: "July 2025 - March 2026",
-
-  //   location: "Semarang, Indonesia",
-
-  //   responsibilities: [
-  //     "Wrote articles, blog posts, product descriptions, advertising materials, and copy for social media, websites, and landing pages based on client requirements.",
-  //     "Adapted writing style to match the target audience and brand identity, creating engaging and effective content.",
-  //     "Conducted topic research and revised content based on client feedback to ensure quality, accuracy, and overall effectiveness.",
-  //   ],
-  // },
 
   {
     id: 2,
@@ -70,13 +30,16 @@ const experienceData = [
 
     logo: freelance_copywriter,
 
-    position: "Data Entry & Copywriter",
+    position: "Copywriter",
 
     duration: "July 2025 - March 2026",
 
     location: "Semarang, Indonesia",
 
     responsibilities: [
+      // "Wrote articles, blog posts, product descriptions, advertising materials, and copy for social media, websites, and landing pages based on client requirements.",
+      // "Adapted writing style to match the target audience and brand identity, creating engaging and effective content.",
+      // "Conducted topic research and revised content based on client feedback to ensure quality, accuracy, and overall effectiveness.",
       "Managed approximately 1,000–5,000 data records per project through data entry, processing, validation, and updating using spreadsheets and databases, while maintaining a high level of accuracy in line with client requirements.",
       "Completed 20+ freelance data entry projects, ensuring consistent formatting, complete information, and timely delivery according to project deadlines.",
       "Produced 30+ pieces of content per month, including blog articles, product descriptions, advertising materials, and social media copy, adapting the writing style to each client’s target audience and brand identity.",
@@ -98,9 +61,12 @@ const experienceData = [
     location: "Tangerang Selatan, Indonesia",
 
     responsibilities: [
-      "Processed cash deposits, withdrawals, transfers, bill payments, and other banking transactions accurately, efficiently, and in accordance with bank procedures.",
-      "Assisted customers by answering inquiries, providing information about Bank Mandiri's products and services, and ensuring a positive customer experience.",
-      "Verified daily transactions, maintained cash security, and prepared financial activity reports in compliance with audit standards and bank policies.",
+      // "Processed cash deposits, withdrawals, transfers, bill payments, and other banking transactions accurately, efficiently, and in accordance with bank procedures.",
+      // "Assisted customers by answering inquiries, providing information about Bank Mandiri's products and services, and ensuring a positive customer experience.",
+      // "Verified daily transactions, maintained cash security, and prepared financial activity reports in compliance with audit standards and bank policies.",
+      "Served approximately 50–150 customers per day for cash deposits, withdrawals, transfers, payments, and other banking services, maintaining a 99% transaction accuracy rate in accordance with Bank Mandiri procedures.",
+      "Processed approximately 100–250 daily transactions while ensuring service efficiency, transaction security, and consistency between system records and supporting documents.",
+      "Responsible for transaction verification, reconciliation, and routine financial reporting, maintaining report accuracy, ensuring transaction data consistency, and supporting smooth audit processes.",
     ],
   },
 ];
